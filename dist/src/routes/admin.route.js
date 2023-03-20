17ae55d18fcfcc2a39a0f2d79ca86314f7c75ec3
Aryan Raj <77831132+AryanRajSingh@users.noreply.github.com>
2023-03-20 06:50:06 +0530

    Add files via upload

--- dist/src/routes/admin.route.js
+++ dist/src/routes/admin.route.js
@@ -3,11 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const express_1 = __importDefault(require("express"));
-const utils_1 = require("../utils");
+const middlewares_1 = require("@src/middlewares");
+const controllers_1 = require("@src/controllers");
 const router = express_1.default.Router();
-router.get('/healthChecker', (req, res) => {
-    const message = 'Welcome to blog API';
-    res.send((0, utils_1.response)({ data: null, success: false, error: true, message, status: 200 }));
-});
+router.get('/users', middlewares_1.isAuth, middlewares_1.isAdmin, controllers_1.getUsersController);
 module.exports = router;
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=admin.route.js.map
